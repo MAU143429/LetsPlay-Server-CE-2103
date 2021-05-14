7e5569b22abd4635b191c0dd57489aa51b98ceab
MAU <mau141226@gmail.com>
2021-05-13 20:57:01 -0600

    Project Created

---
+++ Let´s Play Server/Let´s Play Server.cpp
@@ -0,0 +1,12 @@
+﻿// Let´s Play Server.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "Let´s Play Server.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
