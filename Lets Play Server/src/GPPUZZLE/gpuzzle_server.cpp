4f5ed7a6bb2bc6e5e6fd27e46c7de1f91feffe69
MAU <mau141226@gmail.com>
2021-05-25 08:19:28 -0600

    Game Controller Updated

---
+++ Lets Play Server/src/GPPUZZLE/gpuzzle_server.cpp
@@ -0,0 +1,1 @@
+#include "gpuzzle_server.h"
--- Lets Play Server/src/Socket/Server_Manager.h
+++ Lets Play Server/src/Socket/Server_Manager.h
@@ -1,5 +1,30 @@
 #pragma once
+#include <string>
+#include <iostream>
+#include "../Util_JSON/JSON_Management.h"
+#include "../BPGAME/bpgame_server.h"
+
 class Server_Manager
 {
+
+public:
+	void Select_GameController(const string& jsonString) {
+
+		string game = JSON_Management::GetJSONString("game", jsonString);
+		// Crear la respuesta al client justo aca
+
+		if (game == "BPGAME") {
+
+			// Llamar a funcion controladora del bp game
+		}
+		else if (game == "GPUZZLE") {
+
+			// Llamar a funcion controladora del gp_puzzle 
+
+		}
+
+	}
+
+
 };
 
