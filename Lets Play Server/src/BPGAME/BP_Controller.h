59129fc67aea2513b1cd7468de4c0752ec40020d
MAU <mau141226@gmail.com>
2021-05-25 15:15:23 -0600

    bp_box created

---
+++ Lets Play Server/src/BPGAME/BP_Controller.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include "../Util_JSON/JSON_Management.h"
+#include "../DataStructures/Matrix_bp.h"
+
+using namespace std;
+
+class BP_Controller
+{
+
+public: 
+
+	static void Init_BP() {
+
+		static auto bp_matrix = new Matrix_bp(11, 19);
+		
+		
+		
+
+
+	}
+};
+
---
+++ Lets Play Server/src/DataStructures/Matrix_bp.h
@@ -0,0 +1,107 @@
+#pragma once
+#include <iostream>
+#include <cstdlib>
+#include "SimplyList.h"
+#include "bp_Box.h"
+
+
+using namespace std;
+
+
+
+class Matrix_bp
+{ 
+public:
+	int matrixRows, matrixColumns;
+    SimplyLinkedList<SimplyLinkedList<bp_Box*>*>* bpMatrix;
+	
+
+	Matrix_bp(int lenghtRow, int lenghtColumn) {
+
+		SimplyLinkedList<bp_Box*>* newListRow;
+		this->matrixRows = lenghtRow;
+		this->matrixColumns = lenghtColumn;
+
+		this->bpMatrix = new SimplyLinkedList<SimplyLinkedList<bp_Box*>*>();
+		
+
+		newListRow = createBlankRow(lenghtColumn);
+		
+
+
+		for (size_t i = 0; i < lenghtRow; i++)
+		{
+			bpMatrix->append(newListRow);
+
+		}
+
+	}
+	SimplyLinkedList<bp_Box*>* createBlankRow(int lenght) {
+		auto rowList = new SimplyLinkedList<bp_Box*>();
+		auto box = new bp_Box();
+
+		for (size_t i = 0; i < lenght; i++)
+		{
+			rowList->append(box);
+		}
+
+		return rowList;
+	}
+
+	int getValor(int pos_i, int pos_j) {
+		int result;
+
+		for (size_t i = 0; i < matrixRows; i++){
+
+			if (i == pos_i) {
+				for (size_t j = 0; j < matrixColumns; j++) {
+					result = bpMatrix->get(i)->get(j)->getValue();
+					return result;
+				}
+			}
+		}
+		cout<<"VALOR NO ENCONTRADO"<<endl; 
+	}
+	void printMatrix() {
+		if (this->matrixRows == 0 and this->matrixColumns == 0) {
+			cout << "[]" << endl;
+		}
+		else
+		{
+			cout << "[ " << endl;
+			for (size_t i = 0; i < matrixRows; i++)
+			{
+				for (size_t j = 0; j < matrixColumns; j++) {
+					if (j == matrixColumns - 1) {
+						cout << bpMatrix->get(i)->get(j)->getValue() << " ] ";
+					}
+					else if (j == 0) {
+						cout << " [ " << bpMatrix->get(i)->get(j)->getValue() << " , ";
+					}
+					else{
+						cout << bpMatrix->get(i)->get(j)->getValue() << " , ";
+					}
+				}
+				cout << endl;
+			}
+			cout << " ]" << endl;
+		}
+	}
+
+	void fill_Matrix() {
+
+		int cont = 1;
+
+		for (size_t i = 0; i < matrixRows; i++) {
+
+			for (size_t j = 0; j < matrixColumns; j++) {
+				
+				bpMatrix->get(i)->get(j)->setName(cont);
+				bpMatrix->get(i)->get(j)->setPosx(i);
+				bpMatrix->get(i)->get(j)->setPosy(j);
+				cont++;
+			}
+		}
+	}
+};
+
