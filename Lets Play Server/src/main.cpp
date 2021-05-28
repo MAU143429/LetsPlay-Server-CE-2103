8937d776e5d3fc3ecbfeba1169ac30a466fa5aa7
Naheem Johnson Solís <naheemjohnson@estudiantec.cr>
2021-05-27 22:46:52 -0600

    Added method that serializes typemessage to json

--- Lets Play Server/src/main.cpp
+++ Lets Play Server/src/main.cpp
@@ -3,14 +3,14 @@
 #include "DataStructures/Matrix_bp.h"
 #include"BPGAME/BP_Controller.h"
 #include "DataStructures/SimplyList.h"
-
+#include "DataStructures/Random_Box.h"
+#include "Util_JSON/TypeMessage.h"
 using namespace std;
 
 int main() {
 	//Server::getInstance()->initServer();
 	BP_Controller::Init_BP("16");
 	
-
 	return 0;
 }
 
